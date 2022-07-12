493c54ca7401f4a08b67820f17121ff44c4231b0
legionalitty <109186072+aqiaa@users.noreply.github.com>
2022-07-13 00:39:55 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+from pystyle import Colors, Colorate, Write
+
+
+print(Colorate.Horizontal(Colors.blue_to_purple, """
+╦ ╦╔═╗╔╗ ╦ ╦╔═╗╔═╗╦╔═  ╔╦╗╔═╗╦  ╔═╗╔╦╗╔═╗╦═╗
+║║║║╣ ╠╩╗╠═╣║ ║║ ║╠╩╗   ║║║╣ ║  ║╣  ║ ║╣ ╠╦╝
+╚╩╝╚═╝╚═╝╩ ╩╚═╝╚═╝╩ ╩  ═╩╝╚═╝╩═╝╚═╝ ╩ ╚═╝╩╚═
+Legionalitty#1337""", 1))
+
+webhook = Write.Input("\nEnter Webhook ->", Colors.blue_to_purple, interval=0.1)
+
+r = requests.get(f"{webhook}")
+if r.status_code == 200:
+  Write.Print(f"Valid Webhook Deleting...", Colors.blue_to_purple, interval=0.1)
+if r.status_code == 404:
+  Write.Print(f"Invalid Wenhook", Colors.blue_to_purple, interval=0.1)
+
+if r.status_code == 200:
+  requests.delete(f"{webhook}")
+  Write.Print(f"\nDeleted Webhook", Colors.blue_to_purple, interval=0.1)
